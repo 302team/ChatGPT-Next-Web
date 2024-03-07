9916e35651e9dd3aabdf2a8c885079b88a3eff95
Jerry Moo <jerrymoo@JerrydeMac-mini.local>
2024-03-07 16:18:38 +0800

    update

--- app/api/config/route.ts
+++ app/api/config/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 import { getServerSideConfig } from "../../config/server";
 
+
 const serverConfig = getServerSideConfig();
 
 // Danger! Do not hard code any secret value here!
