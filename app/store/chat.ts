d52c74a3a2938585d07d1e9db5522b2e452455ef
xujiabin <xujiabin@adswave.co>
2024-04-08 18:54:44 +0800

    cancel save media file

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -853,19 +853,19 @@ export const useChatStore = createPersistStore(
               botMessage.isError = hasError as boolean;
               get().onNewMessage(botMessage);
               ChatControllerPool.remove(session.id, botMessage.id);
-              if (!hasError) {
-                console.log(
-                  "[OpenAi] chat finished, save media file to remote",
-                );
-                get()
-                  .saveMediaToRemote(message, botMessage)
-                  .then((newMessage) => {
-                    console.log(
-                      "[OpenAi] chat finished, save media file to remote end:",
-                      newMessage,
-                    );
-                  });
-              }
+              // if (!hasError) {
+              //   console.log(
+              //     "[OpenAi] chat finished, save media file to remote",
+              //   );
+              //   get()
+              //     .saveMediaToRemote(message, botMessage)
+              //     .then((newMessage) => {
+              //       console.log(
+              //         "[OpenAi] chat finished, save media file to remote end:",
+              //         newMessage,
+              //       );
+              //     });
+              // }
             },
             onError(error) {
               const isAborted = error.message.includes("aborted");
