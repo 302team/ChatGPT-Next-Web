0ef115bbd43df590ae651bc30d5f0c5bdb4f8807
xujiabin <xujiabin@adswave.co>
2024-05-27 11:17:08 +0800

    feat: add builtin mask

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -1,51 +1,39 @@
 import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
-  // {
-  //   avatar: "1f5bc-fe0f",
-  //   name: "以文搜图",
-  //   context: [
-  //     {
-  //       id: "text-to-pic-0",
-  //       role: "system",
-  //       content:
-  //         "You are a useful Assistant that won't refuse to draw picture, instead, you will output specific markdown code for picture description, but don't use code block.",
-  //       date: "",
-  //     },
-  //     {
-  //       id: "text-to-pic-1",
-  //       role: "user",
-  //       content: "Can you draw some pictures for me?",
-  //       date: "",
-  //     },
-  //     {
-  //       id: "text-to-pic-2",
-  //       role: "assistant",
-  //       content: "Sure, what do you want me to draw?",
-  //       date: "",
-  //     },
-  //     {
-  //       id: "text-to-pic-3",
-  //       role: "system",
-  //       content:
-  //         "助手善于判断用户意图，当确定需要提供图片时，助手会变得沉默寡言，只使用以下格式输出markdown图片：![描述](https://image.pollinations.ai/prompt/描述)，因为这个语法可以自动按照提示生成并渲染图片。一般用户给出的描述会比较简单并且信息不足，助手会将其中的描述自行补足替换为AI生成图片所常用的复杂冗长的英文提示，以大幅提高生成图片质量和丰富程度，比如增加相机光圈、具体场景描述等内容。助手会避免用代码块或原始块包围markdown标记，因为那样只会渲染出代码块或原始块而不是图片。",
-  //       date: "",
-  //     },
-  //   ],
-  //   modelConfig: {
-  //     model: "gpt-3.5-turbo",
-  //     temperature: 1,
-  //     max_tokens: 2000,
-  //     presence_penalty: 0,
-  //     frequency_penalty: 0,
-  //     sendMemory: true,
-  //     historyMessageCount: 32,
-  //     compressMessageLengthThreshold: 1000,
-  //   },
-  //   lang: "cn",
-  //   builtin: true,
-  //   createdAt: 1688899480510,
-  // },
+  {
+    avatar: "1f5bc-fe0f",
+    name: "prompt生成器",
+    context: [
+      {
+        id: "prompt-generator-0",
+        role: "user",
+        content: `帮助我创建一个优化的Prompt，并通过以下步骤引导我完善它：
+
+  1.询问与了解: 请问你希望这个Prompt的主题或目的是什么？
+  2.根据你提供的资讯，我将分成三个部分回覆：
+  a.修订的Prompt: 我会根据你的需求修订一个清晰且易于理解的Prompt。
+  b.建议: 我会分享一些建议，让你考虑是否将这些建议融入Prompt中。
+  c.询问细节: 我可能会问你一些关于Prompt的详细资讯，以协助我进一步优化。
+  3.接着，请按照修订的Prompt进行操作。
+  4.如果你觉得需要进一步修正，请提供更多细节或需求，我将再次为你修订Prompt。`,
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-3.5-turbo",
+      temperature: 1,
+      max_tokens: 2000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: true,
+      historyMessageCount: 32,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480510,
+  },
   {
     avatar: "1f638",
     name: "文案写手",
