3ba429d5b932b0633a57160f3e5bde5b576e0fef
xujiabin <xujiabin@adswave.co>
2024-09-02 18:46:19 +0800

    update lang init

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { getServerSideConfig } from "./config/server";
 import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 import Locale from "./locales";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: Locale.Config.title,
@@ -41,7 +42,9 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <script src="/lang.js" async></script>
+        <Script id="lang-js" strategy="beforeInteractive">
+          {`console.log('[lang] init');const langCodeMap={"zh-CN":"cn","en-US":"en","pt-BR":"pt","zh-TW":"tw","ja-JP":"jp","ko-KR":"ko","id-ID":"id","fr-FR":"fr","es-ES":"es","it-IT":"it","tr-TR":"tr","de-DE":"de","vi-VN":"vi","ru-RU":"ru","cs-CZ":"cs","no-NO":"no","ar-SA":"ar","bn-BD":"bn","sk-SK":"sk"};try{let n=window.location.hash;if(n.includes("lang=")){const t=n.split("?").pop()?.split("&").find((n=>n.includes("lang")));if(t){const n=t.split("=")[1];localStorage.setItem("lang",langCodeMap[n]??"en")}}}catch(n){};console.log('[lang] inited');`}
+        </Script>
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
